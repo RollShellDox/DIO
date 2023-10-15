487ec634e078f305874d0af2fd4c08b97c7e4450
RollShellDox <Zc777666cZ@163.com>
2023-10-15 19:04:44 +0800

    10.15：完善

--- Demo/src/cn/edu/swu/zc/APP.java
+++ Demo/src/cn/edu/swu/zc/APP.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Stream;
+
 
 public class APP {
     public static void main(String[] args) throws IOException {
@@ -15,7 +17,8 @@ public class APP {
         Map<Integer,String> sortMap = new TreeMap<>();
         for(String url:urls){
             ImageReaderUltra imageReaderUltra = new ImageReaderUltra();
-            Integer size = imageReaderUltra.download(url,"c:\\images\\");
+//            Stream<String> s = sortMap.values().stream().filter("\\").
+            Integer size = imageReaderUltra.download(url,"c:\\images\\"+url.substring(6,(url.length()-6)));
             sortMap.put(size,url);
         }
 
@@ -31,12 +34,3 @@ public class APP {
         }
     }
 }
-//首先，我们创建了一个名为APP的类，并在其中定义了一个main方法。
-//        在main方法中，我们创建了一个PageReaderUltra对象，并使用其getUrl方法从给定的URL中获取URL列表。
-//        接下来，我们创建了一个名为sortMap的TreeMap对象，用于存储图片的大小及其对应的URL。
-//        然后，我们使用for-each循环遍历URL列表，对于每个URL，我们创建一个ImageReaderUltra对象，并使用其download方法下载图片。下载的图片文件大小将作为键，URL作为值添加到sortMap中。
-//        最后，我们使用TreeMap的键（图片大小）对sortMap进行排序，并将排序后的结果写入到c:\images\images-sorted.txt文件中。
-
-//在这个实现中，我们首先使用TreeMap的entrySet方法创建一个包含所有映射项的列表。
-// 然后，我们使用Java 8的流API对列表进行排序，根据键（图片大小）进行排序。
-// 最后，我们使用try-with-resources语句创建一个BufferedWriter对象，用于将排序后的映射项写入到images-sorted.txt文件中。
--- Demo/src/cn/edu/swu/zc/ImageReaderUltra.java
+++ Demo/src/cn/edu/swu/zc/ImageReaderUltra.java
@@ -24,7 +24,7 @@ public class ImageReaderUltra {
         try (InputStream inputStream = connection.getInputStream();
              BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
              FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-            byte[] buffer = new byte[4096];
+            byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
                 fileOutputStream.write(buffer, 0, bytesRead);
@@ -35,11 +35,7 @@ public class ImageReaderUltra {
 
         return (int) file.length();
     }
-//    在这个示例中，我们使用了Java的HttpURLConnection库来下载图片。我们首先创建一个HttpURLConnection对象，设置请求方法为"GET"，并将setDoOutput(true)以允许输出。然后，我们检查响应码，如果响应码不是"200 OK"，则抛出一个IOException。
-//
-//    接下来，我们使用BufferedInputStream和FileOutputStream将图片数据从输入流写入文件。最后，我们返回文件的大小作为下载的文件大小。
-//
-//    请注意，这个示例仅用于演示目的，实际应用中可能需要进行更多的错误处理和验证。
+
 
     private File createPath(String imageUrl, String directory) throws IOException {
         //TODO:create directories and return file path
@@ -52,8 +48,6 @@ public class ImageReaderUltra {
         }
         return file;
 
-//        在这个示例中，我们首先将图片URL分割成路径部分，然后提取文件名。接下来，我们创建一个File对象，该对象表示要下载的图片文件。最后，我们检查父目录是否存在，如果不存在，则使用mkdirs()方法创建目录。
-//
-//        请注意，这个示例仅用于演示目的，实际应用中可能需要进行更多的错误处理和验证
+
     }
 }
